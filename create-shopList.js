0dd88a8c1c22ceef5f75eec5977850229b436575
A67645 <a67645@alunos.uminho.pt>
2020-11-23 15:55:50 +0000

    shopList

---
+++ create-shopList.js
@@ -0,0 +1,50 @@
+// //Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/shopList';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error...'));
+db.once('open', function() {
+    console.log("Conexão ao MongoDB realizada com sucesso...")
+});
+
+var shopListSchemaSchema = new mongoose.Schema({
+    Product: String,
+    Quantity: Number,
+    Category: String
+});
+
+var shopListModel = mongoose.model('list', shopListSchemaSchema)
+
+var data = [
+    {
+      "Product": "Bananas",
+      "Quantity": "6",
+      "Category": "Fruit"
+    },
+     {
+        "Product": "Sugar",
+        "Quantity": "2",
+        "Category": "Groceries"
+    },
+    {
+        "Product": "Apples",
+        "Quantity": "10",
+        "Category": "Fruit"
+    },
+    {
+        "Product": "Carrots",
+        "Quantity": "8",
+        "Category": "Begetables"
+    }
+  ]
+
+shopListModel.create(data)
+
+console.log("That's all folks...")
